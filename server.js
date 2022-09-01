f53cdb8730c96800abe29b70cab6a7cb64cdc66c
christy313 <christycreativity@gmail.com>
2022-09-01 17:21:10 -0400

    install bcrypt

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const database = {
 };
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
@@ -39,6 +39,50 @@ app.post("/signin", (req, res) => {
     res.status(400).json("error logging in");
   }
   res.json("signing");
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+
+  database.users.push({
+    id: "3",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
 });
 
 app.listen(8080, () => {
