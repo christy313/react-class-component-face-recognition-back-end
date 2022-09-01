f816502c5b64a2c72a7602fd11d3b96e21a83203
christy313 <christycreativity@gmail.com>
2022-09-01 16:51:32 -0400

    Test signin route in Postman

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "Mary",
+      email: "mary@gmail.com",
+      password: "hi",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "2",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "hello",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
   res.send("hi");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+  res.json("signing");
 });
 
 app.listen(8080, () => {
