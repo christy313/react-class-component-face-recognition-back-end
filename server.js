c1bd1e158a03c8c256f6e2ef51f98b7c0ffc0aeb
christy313 <christycreativity@gmail.com>
2022-09-01 20:28:41 -0400

    Connect to front-end & add cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -43,6 +47,9 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function (err, hash) {
+    // Store hash in your password DB.
+  });
 
   database.users.push({
     id: "3",
@@ -84,6 +91,14 @@ app.post("/image", (req, res) => {
     res.status(400).json("not found");
   }
 });
+
+// Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function (err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function (err, res) {
+//   // res = false
+// });
 
 app.listen(8080, () => {
   console.log("app is running on port 8080");
