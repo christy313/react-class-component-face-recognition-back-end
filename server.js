e46eddf7ddef05f4775b3bdd9d6b4ac278ea881d
christy313 <christycreativity@gmail.com>
2022-09-03 14:38:38 -0400

    Update knex entries method

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.put("/image", (req, res) => {
     .increment("entries", 1)
     .returning("entries")
     .then((entries) => {
-      res.json(entries[0]);
+      res.json(entries[0].entries);
     })
     .catch((err) => res.status(400).json("unable to get entries"));
 });
