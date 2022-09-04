e9e1239974a1aa14d6074e422048670b6482ecdc
christy313 <christycreativity@gmail.com>
2022-09-03 20:32:13 -0400

    Refactor register function

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const knex = require("knex");
 const app = express();
 
+const register = require("./controllers/register");
+
 const db = knex({
   client: "pg",
   connection: {
@@ -50,31 +52,7 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to register"));
+  register.handlerRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
