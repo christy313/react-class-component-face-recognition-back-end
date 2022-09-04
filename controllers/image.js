1791a7c33a32326ff1624b44bc8269d429b5e719
christy313 <christycreativity@gmail.com>
2022-09-04 11:45:37 -0400

    Move Clarifai Api key to back-end

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,30 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "3261c928d20848b6a4b8674d822130a8",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
 
-  db("users")
+  return db("users")
     .where("id", "=", id)
     .increment("entries", 1)
     .returning("entries")
-    .then((entries) => {
-      res.json(entries[0].entries);
-    })
+    .then((entries) => res.json(entries[0].entries))
     .catch((err) => res.status(400).json("unable to get entries"));
 };
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const { handlerRegister } = require("./controllers/register");
 const { handleSignIn } = require("./controllers/signin");
 const { handleProfile } = require("./controllers/profile");
-const { handleImage } = require("./controllers/image");
+const { handleImage, handleApiCall } = require("./controllers/image");
 
 const db = knex({
   client: "pg",
@@ -25,9 +25,14 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send("success"));
+
 app.post("/signin", (req, res) => handleSignIn(req, res, db, bcrypt));
+
 app.post("/register", (req, res) => handlerRegister(req, res, db, bcrypt));
+
 app.get("/profile/:id", (req, res) => handleProfile(req, res, db));
+
 app.put("/image", (req, res) => handleImage(req, res, db));
+app.post("/imageurl", (req, res) => handleApiCall(req, res));
 
 app.listen(8080, () => console.log("app is running on port 8080"));
